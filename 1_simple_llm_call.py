1a2c9d1ba891c018baeef79b7a9dc56462437fc7
CampusX <53361867+campusx-official@users.noreply.github.com>
2025-08-15 17:42:38 +0530

    Update 1_simple_llm_call.py

--- 1_simple_llm_call.py
+++ 1_simple_llm_call.py
@@ -5,12 +5,6 @@ load_dotenv()
 
 model = ChatOpenAI()
 
-config = {'run_name': 'llm_test', 'tags': ['nitish', 'campusx'], "metadata": {
-        "user_id": 12345,
-        "source": "cli_test",
-        "env": "dev"
-    }}
-
 response = model.invoke('What is the capital of Peru', config=config)
 
 print(response.content)
